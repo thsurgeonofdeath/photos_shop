a7642289d7057b5bcd3beb45f5318824aa878dd4
thsurgeonofdeath <salahek25@gmail.com>
2023-02-21 14:32:52 +0100

    add & remove from cart

--- src/Context.js
+++ src/Context.js
@@ -4,6 +4,7 @@ const Context = React.createContext()
 
 function ContextProvider({children}){
     const [allPhotos,setAllPhotos] = useState([])
+    const [cartItems,setCartItems] = useState([])
 
     const url = "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json"
 
@@ -24,8 +25,16 @@ function ContextProvider({children}){
         setAllPhotos(updatetArr)
     }
 
+    function addToCart(newItem){
+        setCartItems(prevItems => [...prevItems, newItem])
+    }
+
+    function removeFromCart(id){
+        setCartItems(prevItems => prevItems.filter(item => item.id != id))
+    }
+
     return(
-        <Context.Provider value={{allPhotos, toggleFavorite}}>
+        <Context.Provider value={{allPhotos, cartItems, toggleFavorite, addToCart, removeFromCart}}>
             {children}
         </Context.Provider>
     )
--- src/components/Image.js
+++ src/components/Image.js
@@ -5,7 +5,9 @@ import PropTypes from "prop-types"
 
 function Image({className, img}){
     const [hovered, setHovered] = useState(false)
-    const {toggleFavorite} = useContext(Context)
+    const {toggleFavorite, addToCart, cartItems, removeFromCart} = useContext(Context)
+
+    
 
     function heartIcon(){
         if(img.isFavorite){
@@ -13,11 +15,20 @@ function Image({className, img}){
                         onClick={() => toggleFavorite(img.id) }></i>
         }else if(hovered){
             return <i className="ri-heart-line favorite" 
-            onClick={() => toggleFavorite(img.id) }></i>
+                        onClick={() => toggleFavorite(img.id) }></i>
         }
     }
 
-    const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>
+    function cartIcon(){
+        const alreadyInCart = cartItems.some(item => item.id === img.id)
+        if(alreadyInCart){
+            return <i className="ri-shopping-cart-fill cart" 
+                        onClick={() => removeFromCart(img.id)}></i>
+        }else if(hovered){
+            return <i className="ri-add-circle-line cart" 
+                        onClick={() => addToCart(img)}></i>
+        }
+    }
     
     return(
         <div className={`${className} image-container`}
@@ -25,7 +36,7 @@ function Image({className, img}){
         onMouseLeave={() => setHovered(false)}>
             <img src={img.url} className="image-grid" alt=""/>  
             {heartIcon()}
-            {cartIcon}
+            {cartIcon()}
         </div>
     )
 }
